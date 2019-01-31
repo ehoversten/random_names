e981b31807fe23d1f387512ec21c8a55934764e8
ehoversten <ehoversten@gmail.com>
2019-01-31 18:22:30 -0500

    Initial Commit

---
+++ cold_call/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cold_call
+//
+//  Created by Erik Hoversten on 1/31/19.
+//  Copyright © 2019 Erik Hoversten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
